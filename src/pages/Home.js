4970bfaf7f36a79418b86c7c904faa8d9fb603a7
Eric JB <105235872+EB-Joel@users.noreply.github.com>
2022-12-30 12:44:40 -0500

    added Exercise card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,12 @@ import Exercises from '../components/Exercises';
 
 const Home = () => {
   const [bodyPart, setBodyPart] = useState('all')
-  const [exercise, setExercises] = useState([]);
+  const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <HeroBanner />
       <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-      <Exercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} bodyPart={bodyPart} exercises={exercises} />
     </Box>
   )
 }
