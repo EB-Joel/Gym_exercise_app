ce322bf6cd7c491ebf6da41210e4abd3f8d9e549
Eric JB <105235872+EB-Joel@users.noreply.github.com>
2023-01-02 12:51:50 -0500

    imported icons

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -2,8 +2,23 @@ import React from 'react'
 import {Stack, Typography} from '@mui/material';
 
 import all_Icon from '../assets/icons/all.png';
+import back_Icon from '../assets/icons/back.png';
+import cardio_Icon from '../assets/icons/cardio.png';
+import chest_Icon from '../assets/icons/chest.png';
+import lower_arms_Icon from '../assets/icons/lower arms.png';
+import lower_legs_Icon from '../assets/icons/lower legs.png';
+import neck_Icon from '../assets/icons/neck.png';
+import waist_Icon from '../assets/icons/waist.png';
+import upper_arms_Icon from '../assets/icons/upper arms.png';
+import upper_legs_Icon from '../assets/icons/upper legs.png';
+import shoulders_Icon from '../assets/icons/shoulders.png';
 
 const BodyPart = ({item, setBodyPart, bodyPart}) => {
+console.log({item})
+  let img = all_Icon
+   if ({item}.toString() === 'back') { img = back_Icon}
+    else{img =cardio_Icon};
+
   return (
     <Stack
     type="button"
@@ -25,9 +40,10 @@ const BodyPart = ({item, setBodyPart, bodyPart}) => {
         window.scrollTo({top: 1800, left: 100, behavior:'smooth'})
       }}
     >
-        <img src={all_Icon} alt={item} style={{width:'40px', height:'40px'}} />
+        <img src={img} alt={item} style={{width:'40px', height:'40px'}} />
         <Typography fontSize="24px" fontWeight='bold' color="#3A1212" textTransform='capitalize'>{item}</Typography>
     </Stack>
+    
   )
 }
 
